a32026d048b4798475428dba8c1a96ab4a0ad3f9
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-09 00:06:40 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,85 @@
-class TweetsController < ApplicationController
+require 'rack-flash'
 
+class TweetsController < ApplicationController
+  use Rack::Flash
+
+  get '/tweets' do
+    if logged_in?
+      @user = current_user
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if !params[:content].empty?
+      @tweet = Tweet.create(content: params[:content], user: current_user)
+      redirect "/tweets/#{@tweet.id}"
+    else
+      flash[:message] = "Empty text field, please try again."
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    if !params[:content].empty?
+      @tweet.update(:content => params[:content])
+      redirect "/tweets/#{@tweet.id}"
+    else
+      flash[:message] = "Empty text field, please try again."
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    if logged_in? && @tweet.user == current_user
+      @tweet.delete
+      redirect '/tweets'
+    elsif logged_in? && @tweet.user != current_user
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,57 @@
-class UsersController < ApplicationController
+require 'bcrypt'
+require 'rack-flash'
 
+class UsersController < ApplicationController
+  use Rack::Flash
+
+  get '/signup' do
+
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      flash[:message] = "Signup unsuccessful. Please try again."
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      flash[:message] = "Login unsuccessful. Please try again."
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
+    if Helpers.is_logged_in?(session)
+      erb :'users/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if Helpers.is_logged_in?(session)
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
 
 end
