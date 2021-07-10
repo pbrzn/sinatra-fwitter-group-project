7482cc8466448f74fd4e3852b4a24d70d0dadbdb
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-10 12:48:42 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     @tweets = @user.tweets
-    erb :'users/show'
+     erb :'users/show'
   end
 
   get '/logout' do
